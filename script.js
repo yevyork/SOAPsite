cd2d2a331b76d74af507f9a84ecd4e9d91563703
Yev <yevyork@gmail.com>
2019-10-31 02:29:14 -0400

    added popup/animation, started event handler

---
+++ script.js
@@ -0,0 +1,8 @@
+let popup = document.querySelector('.close');
+let closePopup = function(){
+    popup.classList.toggle('.close');
+    }, 250);
+    popup.addEventListener('click')
+}
+
+closePopup()
