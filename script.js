709d0d8555bf1dc2e8256f51e156ccd072063052
Yev <yevyork@gmail.com>
2019-10-31 11:17:12 -0400

    added events

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let popup = document.querySelector('.close');
-let closePopup = function(){
-    popup.classList.toggle('.close');
-    }, 250);
+let popup = document.querySelector('.popcontainer');
+let xButton = document.querySelector('.close')
+    xButton.addEventListener('click', function() {
+        popup.remove();
+    })
 
-
+let signUp = document.querySelector('button');
+    signUp.addEventListener('click', function() {
+        popup.remove()
+        alert("Thanks! Check your inbox and confirm your e-mail address!")
+    })
